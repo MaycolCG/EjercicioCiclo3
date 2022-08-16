ccbc1b6f72ce37f7db44fd35b445252668e08f6a
Maycol Carroll <carrollmaycol@gmail.com>
2022-08-16 15:09:49 +0200

    Hola

--- src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
+++ src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
@@ -2,6 +2,7 @@ package com.UdeA.Ciclo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,5 @@ public class Ciclo3Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
 
-		System.out.println("Hola Mundo");
 	}
 }
