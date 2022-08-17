912e739f574941966f44c887ce622091c9062de5
Maycol Carroll <carrollmaycol@gmail.com>
2022-08-17 15:09:46 +0200

    Update Empleado.java
    Sprint 2: Modelar clase empleado

--- src/main/java/com/UdeA/Ciclo3/Modelos/Empleado.java
+++ src/main/java/com/UdeA/Ciclo3/Modelos/Empleado.java
@@ -1,4 +1,67 @@
 package com.UdeA.Ciclo3.Modelos;
 
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "Empleados")//Crear Tabla
 public class Empleado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String nombre;
+    private String correo;
+    private String empresa;
+    private String rol;
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombre, String correo, String empresa, String rol) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.empresa = empresa;
+        this.rol = rol;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
 }
