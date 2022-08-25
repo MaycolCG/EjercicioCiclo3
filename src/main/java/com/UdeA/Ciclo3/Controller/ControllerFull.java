fc84b6f3021cc51525172f8d54ed23ddfa8de5e4
Maycol Carroll <carrollmaycol@gmail.com>
2022-08-25 14:58:22 +0200

    Clase 25/08
    Terminado Agregar, Editar, Eliminar

--- src/main/java/com/UdeA/Ciclo3/Controller/ControllerFull.java
+++ src/main/java/com/UdeA/Ciclo3/Controller/ControllerFull.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -16,26 +17,52 @@ public class ControllerFull {
     @Autowired
     EmpresaService empresaService;
 
-    @GetMapping ({"/","/VerEmpresas"})
-    public String viewEmpresas(Model model){
-        List<Empresa> listaEmpresas=empresaService.getAllEmpresas();
-        model.addAttribute("emplist",listaEmpresas);
+    @GetMapping({"/", "/VerEmpresas"})
+    public String viewEmpresas(Model model) {
+        List<Empresa> listaEmpresas = empresaService.getAllEmpresas();
+        model.addAttribute("emplist", listaEmpresas);
         return "verEmpresas";
     }
 
     @GetMapping("/AgregarEmpresa")
-    public String nuevaEmpresa(Model model){
-        Empresa emp= new Empresa();
-        model.addAttribute("emp",emp);
+    public String nuevaEmpresa(Model model) {
+        Empresa emp = new Empresa();
+        model.addAttribute("emp", emp);
         return "agregarEmpresa";
     }
 
     @PostMapping("/GuardarEmpresa")
-    public String guardarEmpresa(Empresa emp, RedirectAttributes redirectAttributes){
-        if(empresaService.saveOrUpdateEmpresa(emp)==true){
+    public String guardarEmpresa(Empresa emp, RedirectAttributes redirectAttributes) {
+        if (empresaService.saveOrUpdateEmpresa(emp)) {
             return "redirect:/VerEmpresas";
         }
         return "redirect:/AgregarEmpresa";
     }
 
+    @GetMapping("/EditarEmpresa/{id}")
+    public String editarEmpresa(Model model, @PathVariable Integer id) {
+        Empresa emp = empresaService.getEmpresaById(id);
+        model.addAttribute("emp", emp);
+        return "editarEmpresa";
+    }
+
+    @PostMapping("/ActualizarEmpresa")
+    public String updateEmpresa(Empresa emp, RedirectAttributes redirectAttributes) {
+        if (empresaService.saveOrUpdateEmpresa(emp)) {
+            return "redirect:/VerEmpresas";
+        }
+        return "redirect:/EditarEmpresa";
+    }
+
+    @GetMapping("/EliminarEmpresa/{id}")
+    public String eliminarEmpresa(@PathVariable Integer id) {
+        try {
+            empresaService.deleteEmpresa(id);
+            return "redirect:/VerEmpresas";
+        } catch (Exception e) {
+            return "redirect:/VerEmpresas";
+
+        }
+
+    }
 }
