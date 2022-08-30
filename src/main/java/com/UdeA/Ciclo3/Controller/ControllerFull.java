4c6c505353b14404731e5bbd8a180e5c229d9652
Maycol Carroll <carrollmaycol@gmail.com>
2022-08-30 15:30:53 +0200

    Se Terminan los controladores de Empresa

--- src/main/java/com/UdeA/Ciclo3/Controller/ControllerFull.java
+++ src/main/java/com/UdeA/Ciclo3/Controller/ControllerFull.java
@@ -30,4 +30,24 @@ public class ControllerFull {
         return this.empresaService.getEmpresaById(id);
     }
 
+    @PatchMapping("/enterprises/{id}")
+    public Empresa actualizarEmpresa(@PathVariable("id") Integer id, @RequestBody Empresa empresa) {
+        Empresa emp = empresaService.getEmpresaById(id);
+        emp.setNombre(empresa.getNombre());
+        emp.setDireccion(empresa.getDireccion());
+        emp.setTelefono(empresa.getTelefono());
+        emp.setNIT(empresa.getNIT());
+        return this.empresaService.saveOrUpdateEmpresa(emp);
+    }
+
+    @DeleteMapping (path= "enterprises/{id}") //Eliminar registro de la bd
+    public String DeleteEmpresa(@PathVariable("id") Integer id){
+        boolean respuesta= this.empresaService.deleteEmpresa(id);
+        if (respuesta){  //Si respuesta es true?
+            return "Se elimino la empresa con id" +id;
+        }
+        else{
+            return "No se pudo eliminar la empresa con id"+id;
+        }
+    }
 }
